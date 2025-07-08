4716a89accfca096e98ee8c268d6771518fdca23
johnson <johnsonfelix94@gmail.com>
2025-07-08 22:23:03 +0530

    any disable

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
